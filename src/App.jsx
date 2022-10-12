e08b8e46253bf40749aeb23e284c3839f8127526
Jhan Bayer <jhanbayer@gmail.com>
2022-10-11 22:57:28 -0300

    feat: First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { FaBeer } from "react-icons/fa";
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-red-500 flex justify-center items-center">
+      <h1>Hello World</h1>
+      <FaBeer />
+    </div>
+  );
+}
+
+export default App;
