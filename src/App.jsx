0a2c58f103392bbc9e2c5fb9aaf9e4ea9590d99b
Jhan Bayer <jhanbayer@gmail.com>
2022-10-12 15:56:08 -0300

    style(app): Add padding in whole app

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import AppointmentInfo from "./components/AppointmentInfo";
 
 function App() {
   return (
-    <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl">
+    <div className="App container mx-auto mt-3 font-thin p-5">
+      <h1 className="text-5xl mb-5">
         <BiCalendar className="inline-block text-red-400 align-top" />
         Your Appointments
       </h1>
