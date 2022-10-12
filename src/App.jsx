f6260fdf2b5c6005424790a536d0992b22084b1a
Jhan Bayer <jhanbayer@gmail.com>
2022-10-12 15:33:44 -0300

    refactor(appointment): Add component AppointmentInfo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { BiCalendar, BiTrash } from "react-icons/bi";
+import { BiCalendar } from "react-icons/bi";
 import Search from "./components/Search";
 import AddAppointment from "./components/AddAppointment";
 import appointmentList from "./data.json";
+import AppointmentInfo from "./components/AppointmentInfo";
 
 function App() {
   return (
@@ -14,29 +15,7 @@ function App() {
       <Search />
       <ul className="divide-y divide-gray-200">
         {appointmentList.map((appointment) => (
-          <li className="px-3 py-3 flex items-start" key={appointment.id}>
-            <button
-              type="button"
-              className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-            >
-              <BiTrash />
-            </button>
-            <div className="flex-grow">
-              <div className="flex items-center">
-                <span className="flex-none font-medium text-2xl text-blue-500">
-                  {appointment.petName}
-                </span>
-                <span className="flex-grow text-right">
-                  {appointment.aptDate}
-                </span>
-              </div>
-              <div>
-                <b className="font-bold text-blue-500">Owner:</b>{" "}
-                {appointment.ownerName}
-              </div>
-              <div className="leading-tight">{appointment.aptNotes}</div>
-            </div>
-          </li>
+          <AppointmentInfo appointment={appointment} key={appointment.id} />
         ))}
       </ul>
     </div>
