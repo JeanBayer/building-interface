901f08566016b6ee46df798322c068dab95c16b9
Jhan Bayer <jhanbayer@gmail.com>
2022-10-11 23:14:37 -0300

    feat(search): Add Search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { FaBeer } from "react-icons/fa";
+import { BiArchive } from "react-icons/bi";
+import Search from "./components/Search";
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-red-500 flex justify-center items-center">
-      <h1>Hello World</h1>
-      <FaBeer />
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl">
+        <BiArchive className="inline-block text-red-400 align-top" />
+        Your Appointments
+      </h1>
+      <Search />
     </div>
   );
 }
